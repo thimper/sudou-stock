aabc27c3f21895c3fb7d4d7495386b47032582d5
xiqing.lau <xiqing.lau@hotmail.com>
2015-11-22 10:29:39 +0800

    new created

---
+++ sudou-stock/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sudou_stock
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            winLogin l = new winLogin();
+            if(l.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new Main());
+            }
+            
+        }
+    }
+}
